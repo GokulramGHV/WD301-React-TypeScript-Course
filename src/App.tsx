d2bb91fff02c0054312d3ac4e0b0515b5fd38a2f
Gokulram <gokulram2010418@ssn.edu.in>
2022-03-10 19:50:09 +0530

    formatting changes

--- src/App.tsx
+++ src/App.tsx
@@ -14,27 +14,26 @@ function App() {
 	return (
 		<AppContainer>
 			<div className="p-4 mx-auto bg-white shadow-lg rounded-xl">
-
-					<Header title={'React & TypeScript Course with Tailwind CSS'} />
-					<form action="" className='grid grid-cols-1'>
-						{formFields.map((field) => (
-							<React.Fragment key={field.id}>
-								<label className="">{field.label}</label>
-								<input
-									type={field.type}
-									className="border-2 border-gray-300 rounded-lg p-2 mt-1 mb-2 smooth-effect hover:border-blue-400 hover:ring-blue-400 focus:ring-blue-400 focus:border-blue-400"
-									required
-								/>
-							</React.Fragment>
-						))}
-						<button
-							className="w-28 bg-blue-500 font-medium font-worksans rounded-lg px-4 py-2 my-2 text-white hover:bg-blue-700 smooth-effect"
-							type="submit"
-						>
-							Submit
-						</button>
-					</form>
-				</div>
+				<Header title={'React & TypeScript Course with Tailwind CSS'} />
+				<form action="" className="grid grid-cols-1">
+					{formFields.map((field) => (
+						<React.Fragment key={field.id}>
+							<label className="">{field.label}</label>
+							<input
+								type={field.type}
+								className="border-2 border-gray-300 rounded-lg p-2 mt-1 mb-2 smooth-effect hover:border-blue-400 hover:ring-blue-400 focus:ring-blue-400 focus:border-blue-400"
+								required
+							/>
+						</React.Fragment>
+					))}
+					<button
+						className="w-28 bg-blue-500 font-medium font-worksans rounded-lg px-4 py-2 my-2 text-white hover:bg-blue-700 smooth-effect"
+						type="submit"
+					>
+						Submit
+					</button>
+				</form>
+			</div>
 		</AppContainer>
 	);
 }
