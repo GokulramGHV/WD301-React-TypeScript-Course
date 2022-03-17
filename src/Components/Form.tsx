2655bc62d590482af6035c8f4a7a41a7ef10148b
Gokulram <gokulram2010418@ssn.edu.in>
2022-03-17 21:22:50 +0530

    added option to reset fields

--- src/Components/Form.tsx
+++ src/Components/Form.tsx
@@ -2,32 +2,106 @@ import React, { useState } from 'react';
 import UserInput from './UserInput';
 
 const formFields = [
-  { id: 1, label: 'First Name', type: 'text' },
-  { id: 2, label: 'Last Name', type: 'text' },
-  { id: 3, label: 'Email', type: 'email' },
-  { id: 4, label: 'Date of Birth', type: 'date' },
-  { id: 5, label: 'Phone Number', type: 'number' },
+  { id: 1, label: 'First Name', type: 'text', value: '' },
+  { id: 2, label: 'Last Name', type: 'text', value: '' },
+  { id: 3, label: 'Email', type: 'email', value: '' },
+  { id: 4, label: 'Date of Birth', type: 'date', value: '' },
+  { id: 5, label: 'Phone Number', type: 'number', value: '' },
 ];
 
 export function Form(props: { closeFormCB: () => void }) {
+  const [newField, setNewField] = useState('');
   const [state, setState] = useState(formFields);
   const addField = () => {
-    setState([
-      ...state,
-      {
-        id: state.length + 2,
-        label: 'New Field',
-        type: 'text',
-      },
-    ]);
+    if (newField != '') {
+      setState([
+        ...state,
+        {
+          id: Number(new Date()),
+          label: newField,
+          type: 'text',
+          value: '',
+        },
+      ]);
+      setNewField('');
+    }
+  };
+
+  const removeField = (id: number) => {
+    setState(state.filter((field) => field.id !== id));
+  };
+
+  const resetFields = () => {
+    let arr: any = [];
+    state.forEach((s) => {
+      let obj = {
+        ...s,
+        value: '',
+      };
+      arr.push(obj);
+    });
+    setState(arr);
+  };
+
+  const onChangeField = (val: string, id: number) => {
+    let arr: any = [];
+    state.forEach((s) => {
+      if (s.id == id) {
+        let obj = {
+          ...s,
+          value: val,
+        };
+        arr.push(obj);
+      } else {
+        arr.push(s);
+      }
+    });
+    setState(arr);
   };
 
   return (
-    <form className="grid grid-cols-1">
-      {state.map((field) => (
-        <UserInput key={field.id} label={field.label} type={field.type} />
-      ))}
+    <div className="grid grid-cols-1 divide-y-2">
+      <div className="mb-2">
+        {state.map((field) => (
+          <UserInput
+            id={field.id}
+            key={field.id}
+            label={field.label}
+            type={field.type}
+            value={field.value}
+            removeFieldCB={removeField}
+            onChangeCB={onChangeField}
+          />
+        ))}
+      </div>
+
       <div>
+        <div className="flex w-full mt-2">
+          <input
+            onChange={(e) => {
+              setNewField(e.target.value);
+            }}
+            value={newField}
+            placeholder="Enter field name"
+            type="text"
+            className=" flex-1 border-2 border-gray-300 rounded-lg p-2 mt-1 mb-2 smooth-effect hover:border-blue-400 hover:ring-blue-400 focus:ring-blue-400 focus:border-blue-400"
+          />
+
+          <button
+            className="ml-3 w-32 bg-blue-500 font-medium font-worksans rounded-lg px-4 py-2 my-2 text-white hover:bg-blue-700 smooth-effect"
+            onClick={addField}
+          >
+            Add Field
+          </button>
+        </div>
+
+        <button
+          className="mr-3 w-28 bg-blue-500 font-medium font-worksans rounded-lg px-4 py-2 my-2 text-white hover:bg-blue-700 smooth-effect"
+          onClick={resetFields}
+        >
+          Reset
+        </button>
+
         <button
           className="w-28 bg-blue-500 font-medium font-worksans rounded-lg px-4 py-2 my-2 text-white hover:bg-blue-700 smooth-effect"
           type="submit"
@@ -40,14 +114,7 @@ export function Form(props: { closeFormCB: () => void }) {
         >
           Close Form
         </button>
-
-        <button
-          className="ml-3 w-32 bg-blue-500 font-medium font-worksans rounded-lg px-4 py-2 my-2 text-white hover:bg-blue-700 smooth-effect"
-          onClick={addField}
-        >
-          Add Field
-        </button>
       </div>
-    </form>
+    </div>
   );
 }
--- src/Components/UserInput.tsx
+++ src/Components/UserInput.tsx
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function UserInput(props: { key: number, label: string, type: string }) {
+export default function UserInput(props: {
+  id: number;
+  label: string;
+  type: string;
+  value: string;
+  removeFieldCB: (id: number) => void;
+  onChangeCB: (val: string, id: number) => void;
+}) {
   return (
-    <React.Fragment key={props.key}>
+    <React.Fragment>
       <label>{props.label}</label>
-      <input
-        type={props.type}
-        className="border-2 border-gray-300 rounded-lg p-2 mt-1 mb-2 smooth-effect hover:border-blue-400 hover:ring-blue-400 focus:ring-blue-400 focus:border-blue-400"
-        required
-      />
+      <div className="flex">
+        <input
+          type={props.type}
+          value={props.value}
+          onChange={(e) => {
+            let value = e.target.value;
+            props.onChangeCB(value, props.id);
+          }}
+          className="flex-1 border-2 border-gray-300 rounded-lg p-2 mt-1 mb-2 smooth-effect hover:border-blue-400 hover:ring-blue-400 focus:ring-blue-400 focus:border-blue-400"
+        />
+        <button
+          className="ml-3 w-32 bg-blue-500 font-medium font-worksans rounded-lg px-2 py-2 my-2 text-white hover:bg-blue-700 smooth-effect"
+          onClick={(_) => props.removeFieldCB(props.id)}
+        >
+          Remove
+        </button>
+      </div>
     </React.Fragment>
   );
 }
